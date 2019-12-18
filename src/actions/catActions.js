7488e7b5c5cba718b75a9781799e2cc357599670
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 21:41:13 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,8 @@
-// write and export your action creator function here
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:4000/db')
+      .then(response => response.json())
+      .then(cats => dispatch({ type: 'FETCH_CATS', payload: cats.images }));
+  };
+}
