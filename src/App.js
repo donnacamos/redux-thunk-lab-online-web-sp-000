4ccfc04d0bea42836e69b901c683772a0a841f46
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 21:55:59 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCats } from './actions/catActions'
- 
+
 class App extends Component {
- 
+
   componentDidMount() {
     console.log(this.props)
     this.props.fetchCats()
   }
- 
+
   render() {
     console.log(this.props.catPics) // log will fire every time App renders
     return (
@@ -19,14 +19,14 @@ class App extends Component {
     );
   }
 }
- 
+
 const mapStateToProps = state => {
   return {
     catPics: state.cats,
     loading: state.loading
   }
 }
- 
+
 const mapDispatchToProps = dispatch => {
   return {
     fetchCats: () => dispatch(fetchCats())
