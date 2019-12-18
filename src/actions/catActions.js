60a2534380cfd853b9995b5ff4de1b747c7bb4f7
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 21:59:07 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -6,7 +6,7 @@ export function fetchCats(){
 
         return fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
             .then(res => res.json())
-            .then(data => dispatch({type: "FETCH_CATS", payload: data.images}))
+            .then(data => dispatch({type: "ADD_CATS", payload: data.images}))
 
     }
 }
